9dc3707ac53150d0c09f8e55e5acc59a0bed54e0
Giuseppe Crinò <giuscri@gmail.com>
2014-11-27 00:58:01 +0100

    Added a script for fast hack-on setup

---
+++ setupenv_Btclient.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+setupenv_Btclient() {
+    which npm || (echo '
+        [!] You need npm to continue.
+        Check your distribution documentation.'; return);
+    which bower || (echo '
+        [!] You need bower to continue.
+        Check your distribution documentation.'; return);
+    git clone git@github.com:NAGTeam/btclient.git || return;
+    cd btclient;
+    npm install || (echo '
+        [!] npm failed to install dependencies.'; return);
+    bower install || (echo '
+        [!] bower failed to install dependencies.'; return);
+    echo; echo '[+] You are now ready to contribute!';
+}
+
+setupenv_Btclient;
